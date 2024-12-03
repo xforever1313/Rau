dfad95bcd9404891d61bd6edac1e295d77d22212
Seth Hendrick <seth@shendrick.net>
2024-12-02 22:46:48 -0500

    use normal namespacing

--- src/Rss2Pds/ApiBuilder.cs
+++ src/Rss2Pds/ApiBuilder.cs
@@ -1,22 +1,41 @@
+//
+// Rau - A bot that reads RSS feeds and posts them to a AT-Proto PDS node
+// Copyright (C) 2024 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 using Rau.Standard;
 
-namespace Rau;
-
-public abstract class ApiBuilder
+namespace Rau
 {
-    // ---------------- Constructor ----------------
+    public abstract class ApiBuilder
+    {
+        // ---------------- Constructor ----------------
 
-    protected ApiBuilder()
-    {
-    }
-    
-    // ---------------- Methods ----------------
-    
-    public virtual void ConfigureRauSettings( IRauConfigurator configurator )
-    {
-    }
+        protected ApiBuilder()
+        {
+        }
 
-    public virtual void ConfigureBot( IRauApi rau )
-    {
+        // ---------------- Methods ----------------
+
+        public virtual void ConfigureRauSettings( IRauConfigurator configurator )
+        {
+        }
+
+        public virtual void ConfigureBot( IRauApi rau )
+        {
+        }
     }
 }
